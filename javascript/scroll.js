f0cdb9d0e0eaf1fd2c89b975abb6e19bd46147b3
Zachary Deacon <zackdeacon347@gmail.com>
2023-08-17 19:32:54 -0700

    shouldve fixed js

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -2,35 +2,36 @@ $(window).scroll(function() {
     var scrollDistance = $(window).scrollTop();
     console.log("scrolldistance: "+scrollDistance);
 
-    if (scrollDistance < 140) {
+    if (scrollDistance < 120) {
         $('#sunday-tab').removeClass('inView');
         $('#saturday-tab').removeClass('inView');
         $('#friday-tab').removeClass('inView');
+        $('#photos-tab').removeClass('inView');
         $('#sunday-tab').addClass('backToNorm');
         $('#saturday-tab').addClass('backToNorm');
         $('#friday-tab').addClass('backToNorm');
+        $('#photos-tab').addClass('backToNorm');
     }
 
-        if (scrollDistance > 140 && scrollDistance < 580) {
+        if (scrollDistance > 120 && scrollDistance < 580) {
             $('#sunday-tab').removeClass('inView');
             $('#saturday-tab').removeClass('inView');
             $('#friday-tab').removeClass('backToNorm');
+            $('#photos-tab').removeClass('inView');
             $('#friday-tab').addClass('inView');
             $('#sunday-tab').addClass('backToNorm');
             $('#saturday-tab').addClass('backToNorm');
-            $('#photos-tab').removeClass('inView');
             $('#photos-tab').addClass('backToNorm');
-
         }
 
         if (scrollDistance > 580 && scrollDistance < 1050) {
             $('#sunday-tab').removeClass('inView');
             $('#friday-tab').removeClass('inView');
             $('#saturday-tab').removeClass('backToNorm');
+            $('#photos-tab').removeClass('inView');
             $('#saturday-tab').addClass('inView');
             $('#sunday-tab').addClass('backToNorm');
             $('#friday-tab').addClass('backToNorm');
-            $('#photos-tab').removeClass('inView');
             $('#photos-tab').addClass('backToNorm');
 
         }
@@ -39,10 +40,10 @@ $(window).scroll(function() {
             $('#friday-tab').removeClass('inView');
             $('#saturday-tab').removeClass('inView');
             $('#sunday-tab').removeClass('backToNorm');
+            $('#photos-tab').removeClass('inView');
             $('#sunday-tab').addClass('inView');
             $('#friday-tab').addClass('backToNorm');
             $('#saturday-tab').addClass('backToNorm');
-            $('#photos-tab').removeClass('inView');
             $('#photos-tab').addClass('backToNorm');
         }
 
